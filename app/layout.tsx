2f17dde903511f3abd5cfecf256c126a3a3b4648
Michael Zeltner <michael.zeltner@me.com>
2025-04-30 18:42:55 +0200

    feat: add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Lexend_Giga } from "next/font/google";
 import { ColorSchemeScript, mantineHtmlProps } from "@mantine/core";
+import { Analytics } from "@vercel/analytics/next";
 
 import { ThemeProvider } from "@/ui/Theme";
 
@@ -42,6 +43,7 @@ export default function RootLayout({
         <ThemeProvider>
           <Shell>{children}</Shell>
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
